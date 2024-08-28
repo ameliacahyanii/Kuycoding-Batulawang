d0fb6a4debe91af2c0e74a9283945c1feb57f7d6
syiffanoriza <syiffa.noriza@gmail.com>
2024-08-28 23:53:59 +0700

    Adding src/components/navbar.php to all existing views

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Service Detail</title>
+    <title>Wisata Batu Lawang Jaya</title>
+    <link rel="stylesheet" href="../src/styles/main.css">
+    <link rel="stylesheet" href="../src/styles/service-detail.css">
 </head>
+
 <body>
-    
+    <?php $activePage = 'home';
+    include '../src/components/navbar.php'; ?>
 </body>
+
 </html>
--- views/service-detail.php
+++ views/service-detail.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Service</title>
+    <title>Jasa Foto</title>
     <link rel="stylesheet" href="../src/styles/main.css">
     <link rel="stylesheet" href="../src/styles/service-detail.css">
 </head>
