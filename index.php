1ce4c1fbb831b84cfbb2ece84a874e6f849771b5
Amelia Cahyani <amelia.cahyani@digitalcenter.id>
2024-08-29 13:38:24 +0700

    create - Footer Component

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wisata Batu Lawang Jaya</title>
-    <link rel="stylesheet" href="src/styles/main.css">
-    <link rel="stylesheet" href="src/styles/service-detail.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wisata Batu Lawang Jaya</title>
+        <link rel="stylesheet" href="src/styles/main.css">
+        <link rel="stylesheet" href="src/styles/service-detail.css">
+    </head>
 
-<body>
-    <?php $activePage = 'home';
-    include 'src/components/navbar.php'; ?>
-</body>
+    <body>
+        <?php $activePage = 'home';
+        include 'src/components/navbar.php';
+        ?>
+        
+    </body>
+
+    <?php
+    include 'src/components/footer.php';
+    ?>
 
 </html>
